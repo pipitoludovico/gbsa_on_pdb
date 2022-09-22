6bcde0a3e3a35cebf5b4db1ce72d02597e1aefe3
Ludovico PIpitò <67153855+pipitoludovico@users.noreply.github.com>
2022-09-22 11:46:23 +0100

    Add files via upload

--- decomp_reader.py
+++ decomp_reader.py
@@ -20,8 +20,10 @@ def decomp_reader(datfile, ligand, receptor, file_reader):
                     lig_num.append(line_lig[22:27].strip())
                 if line_lig[72:75].strip() not in lig_dec_residue:
                     lig_dec_residue[str(line_lig[72:75].strip())] = {}
-                if (line_lig.split()[3] + " " + line_lig[22:27].strip()) not in lig_dec_residue[str(line_lig[72:75].strip())]:
-                    lig_dec_residue[str(line_lig[72:75].strip())][line_lig.split()[3] + " " + line_lig[22:27].strip()] = 0
+                if (line_lig.split()[3] + " " + line_lig[22:27].strip()) not in lig_dec_residue[
+                    str(line_lig[72:75].strip())]:
+                    lig_dec_residue[str(line_lig[72:75].strip())][
+                        line_lig.split()[3] + " " + line_lig[22:27].strip()] = 0
 
     with open(receptor, 'r') as r:
         header_r = r.readlines()[1:]
@@ -31,30 +33,29 @@ def decomp_reader(datfile, ligand, receptor, file_reader):
                     rec_num.append(line_r[22:27].strip())
                 if line_r[72:75].strip() not in recept_dec_residue:
                     recept_dec_residue[str(line_r[72:75].strip())] = {}
-                if (line_r.split()[3] + " " + line_r[22:27].strip()) not in recept_dec_residue[str(line_r[72:75].strip())]:
+                if (line_r.split()[3] + " " + line_r[22:27].strip()) not in recept_dec_residue[
+                    str(line_r[72:75].strip())]:
                     recept_dec_residue[str(line_r[72:75].strip())][line_r.split()[3] + " " + line_r[22:27].strip()] = 0
 
     with open(datfile, 'r') as f:
-        noheader = f.readlines()[7:]
-
+        noheader = f.readlines()[8:]
+        x = 0
         for lines in noheader:
             if lines == "" or lines == " " or lines == "\n":
                 break
             if lines.split(",")[1][0] == "L":
-                x = 0
                 for key in lig_dec_residue.keys():
                     if (lines.split()[0]) + " " + lig_num[x] in lig_dec_residue[key]:
                         lig_dec_residue[key][(lines.split()[0]) + " " + lig_num[x]] = (lines.split(",")[17])
                     x += 1
-        noheader = f.readlines()[7:]
+
+        y = 0
         for lines in noheader:
             if lines == "" or lines == " " or lines == "\n":
                 break
             if lines.split(",")[1][0] == "R":
-                y = 0
                 for key in recept_dec_residue.keys():
                     if (lines.split()[0]) + " " + rec_num[y] in recept_dec_residue[key]:
                         recept_dec_residue[key][(lines.split()[0]) + " " + rec_num[y]] = (lines.split(",")[17])
                     y += 1
-
     return recept_dec_residue, lig_dec_residue
